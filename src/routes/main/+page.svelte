ef038e23750f82b23120ee49a015134918f67403
sg-hwang-333 <hsg151515@naver.com>
2024-08-23 17:46:05 +0900

    [0823] 이동 수정 (goto -> a태그)

--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { goto } from '$app/navigation';
 	import { pb } from '$lib/pocketbase';
 	import { onMount } from 'svelte';
 
@@ -8,17 +7,13 @@
 		console.log(user);
 		if (user == null) {
 			alert('로그인 후 이용 가능합니다');
-			goto('../login');
 		}
 	});
-
-	const gotoInfo = () => {
-		goto('/main/info');
-	};
 </script>
 
 <h1>메인 페이지</h1>
 
 <ul>
-	<button on:click={gotoInfo}>Info</button>
+	<li><a href="../">Home</a></li>
+	<li><a href="main/info">Info</a></li>
 </ul>
