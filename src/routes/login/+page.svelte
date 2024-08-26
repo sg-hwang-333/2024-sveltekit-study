bcc69ba493a3c4338f4260c81bb14af3a02146a6
sg-hwang-333 <hsg151515@naver.com>
2024-08-26 14:43:43 +0900

    [0826] shadcn과 tailwind 사용

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,6 +3,8 @@
 	import { pb } from '$lib/pocketbase';
 	import { isLogin } from '../stores/isLoginStore';
 
+	import { Input } from '$lib/components/ui/input';
+
 	let email = '';
 	let password = '';
 
@@ -26,16 +28,18 @@
 <h1 class="text-3xl font-bold underline">로그인</h1>
 
 <form on:submit={handleLogin}>
+	<!-- svelte-ignore a11y-label-has-associated-control -->
 	<label>
 		이메일
-		<input type="text" bind:value={email} required />
+		<Input type="text" bind:value={email} required />
 	</label>
 
 	<br /><br />
 
+	<!-- svelte-ignore a11y-label-has-associated-control -->
 	<label>
 		비밀번호
-		<input type="password" bind:value={password} required />
+		<Input type="password" bind:value={password} required />
 	</label>
 
 	<br /><br />
